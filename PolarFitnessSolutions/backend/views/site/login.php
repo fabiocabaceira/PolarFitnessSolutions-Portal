d1e01e254303099264e57c2ff2b09dac4dd0a566
Gabriel <2211901@my.ipleiria.pt>
2022-12-12 17:04:43 +0000

    Alteracoes do visual do login na backend

--- PolarFitnessSolutions/backend/views/site/login.php
+++ PolarFitnessSolutions/backend/views/site/login.php
@@ -1,56 +1,37 @@
 <?php
 use yii\helpers\Html;
 ?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<div class="card">
-    <div class="card-body login-card-body">
-        <p class="login-box-msg">Sign in to start your session</p>
+<div class="col-md-12 d-flex flex-column justify-content-center">
+<div class="col-lg-6 col-md-8 mx-auto">
+    <dilogin-card-bodyv class="card-body login-card-body">
 
-        <?php $form = \yii\bootstrap4\ActiveForm::begin(['id' => 'login-form']) ?>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <?= $form->field($model,'username', [
-            'options' => ['class' => 'form-group has-feedback'],
-            'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-envelope"></span></div></div>',
-            'template' => '{beginWrapper}{input}{error}{endWrapper}',
-            'wrapperOptions' => ['class' => 'input-group mb-3']
-        ])
-            ->label(false)
-            ->textInput(['placeholder' => $model->getAttributeLabel('username')]) ?>
-
-        <?= $form->field($model, 'password', [
-            'options' => ['class' => 'form-group has-feedback'],
-            'inputTemplate' => '{input}<div class="input-group-append"><div class="input-group-text"><span class="fas fa-lock"></span></div></div>',
-            'template' => '{beginWrapper}{input}{error}{endWrapper}',
-            'wrapperOptions' => ['class' => 'input-group mb-3']
-        ])
-            ->label(false)
-            ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
-
-        <div class="row">
-            <div class="col-8">
-                <?= $form->field($model, 'rememberMe')->checkbox([
-                    'template' => '<div class="icheck-primary">{input}{label}</div>',
-                    'labelOptions' => [
-                        'class' => ''
-                    ],
-                    'uncheck' => null
-                ]) ?>
+<div class="back">
+    <div class="div-center">
+        <div class="content">
+            <div class="form-image">
+                <?php $domain = yii\helpers\Url::base(true);
+                echo Html::img($domain.'/imgs/Polar_logo_black.png', ['alt' => 'Imagem do Polar', 'class' => 'image-login img-fluid']) ?>
             </div>
-            <div class="col-4">
-                <?= Html::submitButton('Sign In', ['class' => 'btn btn-primary btn-block']) ?>
+
+                <h3 class="text-center">Backend Login</h3>
+
+            <hr/>
+            <?php $form = \yii\bootstrap4\ActiveForm::begin() ?>
+                <div class="user-box">
+                    <?= $form->field($model,'username')?>
+                </div>
+                <div class="user-box">
+                    <?= $form->field($model,'password')->passwordInput() ?>
+                </div>
+            <spacer type="horizontal" width="100" height="100"> ㅤ </spacer>
+            <div class="d-grid gap-2 col-8 mx-auto">
+            <?= Html::submitButton('Sign In', ['class' => 'btn btn-outline-dark']) ?>
             </div>
+            <?php \yii\bootstrap4\ActiveForm::end(); ?>
         </div>
-
-        <?php \yii\bootstrap4\ActiveForm::end(); ?>
-
-        <!-- /.social-auth-links -->
-
-        <p class="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
-        </p>
-        <p class="mb-0">
-            <a href="register.html" class="text-center">Register a new membership</a>
-        </p>
     </div>
-    <!-- /.login-card-body -->
+</div>
 </div>
