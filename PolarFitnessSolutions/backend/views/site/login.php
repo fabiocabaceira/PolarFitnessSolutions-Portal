764518cbcf56c35e4c12d441e9da5afd6984b0a6
Gabriel <2211901@my.ipleiria.pt>
2022-12-16 20:16:34 +0000

    alteracao do design do login e da pagina inicial

--- PolarFitnessSolutions/backend/views/site/login.php
+++ PolarFitnessSolutions/backend/views/site/login.php
@@ -5,8 +5,7 @@ use yii\helpers\Html;
 <div class="col-lg-6 col-md-8 mx-auto">
     <dilogin-card-bodyv class="card-body login-card-body">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 
 <div class="back">
     <div class="div-center">
--- PolarFitnessSolutions/frontend/views/site/login.php
+++ PolarFitnessSolutions/frontend/views/site/login.php
@@ -8,34 +8,36 @@ use yii\bootstrap5\Html;
 use yii\bootstrap5\ActiveForm;
 
 $this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="col-md-12 d-flex flex-column justify-content-center">
+    <div class="col-lg-6 col-md-8 mx-auto">
+        <dilogin-card-bodyv class="card-body login-card-body">
 
-    <p>Please fill out the following fields to login:</p>
+            <div class="back">
+                <div class="div-center">
+                    <div class="content">
+                        <div class="form-image">
+                            <?php $domain = yii\helpers\Url::base(true);
+                            echo Html::img($domain.'/imgs/Polar_logo_black.png', ['alt' => 'Imagem do Polar', 'class' => 'image-login img-fluid']) ?>
+                        </div>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                        <h3 class="text-center">Seja bem-vindo</h3>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div class="my-1 mx-0" style="color:#999;">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                    <br>
-                    Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
+                        <hr/>
+                        <?php $form = \yii\bootstrap4\ActiveForm::begin() ?>
+                        <div class="user-box">
+                            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                        </div>
+                        <div class="user-box">
+                            <?= $form->field($model,'password')->passwordInput() ?>
+                        </div>
+                        <spacer type="horizontal" width="100" height="100"> ㅤ </spacer>
+                        <div class="d-grid gap-2 col-8 mx-auto">
+                            <?= Html::submitButton('Sign In', ['class' => 'btn btn-outline-dark']) ?>
+                        </div>
+                        <?php \yii\bootstrap4\ActiveForm::end(); ?>
+                    </div>
                 </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
-        </div>
+            </div>
     </div>
-</div>
+
