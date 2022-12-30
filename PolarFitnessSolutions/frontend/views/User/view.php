e66a236ce731d5d9f2dc9376210c7185d21656c1
Gabriel <2211901@myipleiria.pt>
2022-12-30 08:47:30 +0000

    Alteracao visual dos detalhes de conta e formatacao de codigo da pagina de contacto

--- PolarFitnessSolutions/frontend/views/User/view.php
+++ PolarFitnessSolutions/frontend/views/User/view.php
@@ -6,46 +6,71 @@ use yii\widgets\DetailView;
 /** @var yii\web\View $this */
 /** @var frontend\models\User $model */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = $model->username;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="user-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Bem Vindo <?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+    <div class="container d-flex justify-content-center">
+            <div class="col-md-6 border-right">
+                    <div class="d-flex justify-content-between align-items-center mb-3">
+                        <h4 class="text-right">Detalhes</h4>
+                    </div>
+                    <div class="row mt-3 ">
+                        <span class="col-md-12"><?= DetailView::widget([
+                                'model' => $model,
+                                'options' => ['class' => 'col-md-12'],
+                                'template' => '<div class="col-md-12"><label>{label}</label><label class="form-control">{value}</label></div>',
+                                'attributes' => [
+                                    [
+                                        'label' => 'username',
+                                        'value' => $model->username,
+                                    ],
+                                    [
+                                        'label' => 'email',
+                                        'value' => $model->email,
+                                    ],
+                                    [
+                                        'label' => 'created at',
+                                        'value' => $model->created_at,
+                                    ],
+                                    [
+                                        'label' => 'last updated at',
+                                        'value' => $model->updated_at,
+                                    ],
+                                    [
+                                        'label' => 'status',
+                                        'value' => $model->status,
+                                    ],
+                                    [
+                                        'label' => 'street',
+                                        'value' => $model->street,
+                                    ],
+                                    [
+                                        'label' => 'zip code',
+                                        'value' => $model->zip_code,
+                                    ],
+                                    [
+                                        'label' => 'phone_number',
+                                        'value' => $model->phone_number,
+                                    ],
+                                    [
+                                        'label' => 'nif',
+                                        'value' => $model->nif,
+                                    ],
+                                    [
+                                        'label' => 'gender',
+                                        'value' => $model->gender,
+                                    ],
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'username',
-            'password_hash',
-            'email:email',
-            'auth_key',
-            'password_reset_token',
-            'created_at',
-            'updated_at',
-            'verification_token',
-            'status',
-            'street',
-            'zip_code',
-            'area',
-            'phone_number',
-            'nif',
-            'gender',
-        ],
-    ]) ?>
+                                ],
 
+                            ]) ?></span>
+                        <div class="mt-5 text-center">
+                            <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary profile-button']) ?>
+                        </div>
+
+            </div>
 </div>
--- PolarFitnessSolutions/frontend/views/site/contact.php
+++ PolarFitnessSolutions/frontend/views/site/contact.php
@@ -14,13 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-contact">
     <h1><?= Html::encode($this->title) ?></h1>
     <?php $domain = yii\helpers\Url::base(true);
-
-
      echo Html::img($domain.'/imgs/apoioaocliente.jpeg', ['alt' => 'Imagem do Polar', 'class' => 'image-login img-fluid']) ?>
-
-    <p>
-        Inserir texto
-    </p>
 
     <div class="row">
         <div class="col-lg-5">
