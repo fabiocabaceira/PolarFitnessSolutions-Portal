89e0bbcb18a1d504df87cd7e14c760d4f7f21b04
Gabriel <2211901@my.ipleiria.pt>
2023-01-03 12:24:08 +0000

    crud da worker_client_relation na frontend e na backend

---
+++ PolarFitnessSolutions/frontend/models/WorkerClientRelation.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "worker_client_relation".
+ *
+ * @property int $id
+ * @property int|null $client_id
+ * @property int|null $worker_id
+ *
+ * @property Client $client
+ * @property Worker $worker
+ */
+class WorkerClientRelation extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'worker_client_relation';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id', 'client_id', 'worker_id'], 'integer'],
+            [['id'], 'unique'],
+            [['client_id'], 'exist', 'skipOnError' => true, 'targetClass' => Client::class, 'targetAttribute' => ['client_id' => 'client_id']],
+            [['worker_id'], 'exist', 'skipOnError' => true, 'targetClass' => Worker::class, 'targetAttribute' => ['worker_id' => 'worker_id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'client_id' => 'Client ID',
+            'worker_id' => 'Worker ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[Client]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getClient()
+    {
+        return $this->hasOne(Client::class, ['client_id' => 'client_id']);
+    }
+
+    /**
+     * Gets query for [[Worker]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getWorker()
+    {
+        return $this->hasOne(Worker::class, ['worker_id' => 'worker_id']);
+    }
+}
