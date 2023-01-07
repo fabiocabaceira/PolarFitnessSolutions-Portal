e4e901482b477ff0fbd73fcabc8f330a56b55a47
fabiocabaceira <fabiocabaceira@gmail.com>
2023-01-07 16:08:43 +0000

    Alteração dos forms de criação de funcionários e clientes na backend

--- PolarFitnessSolutions/backend/models/WorkerCreateForm.php
+++ PolarFitnessSolutions/backend/models/WorkerCreateForm.php
@@ -70,6 +70,7 @@ class WorkerCreateForm extends Model
         $user->area = $this->area;
         $user->nif = $this->nif;
         $user->gender = $this->gender;
+        $user->status = 10;
         $user->setPassword($this->password);
         $user->generateAuthKey();
         $user->generateEmailVerificationToken();
