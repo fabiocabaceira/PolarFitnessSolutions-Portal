a97c0585292cc747fbe391ccecabdce41bc35f78
Gabriel <2211901@myipleiria.pt>
2023-01-10 23:27:59 +0000

    HomeCest edicao do LoginCest na backend e organizacao testes funcionais frontend

--- PolarFitnessSolutions/frontend/tests/functional/HomeCest.php
+++ PolarFitnessSolutions/frontend/tests/functional/HomeCest.php
@@ -6,12 +6,12 @@ use frontend\tests\FunctionalTester;
 
 class HomeCest
 {
-    public function checkOpen(FunctionalTester $I)
+    public function checkHomeAndRedirectToLogin(FunctionalTester $I)
     {
         $I->amOnRoute(\Yii::$app->homeUrl);
-        $I->see('My Application');
-        $I->seeLink('About');
-        $I->click('About');
-        $I->see('This is the About page.');
+        $I->see('Criar conta');
+        $I->seeLink('Login');
+        $I->click('Login');
+        $I->see('Seja bem-vindo');
     }
 }
